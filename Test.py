f0dab54db84ecef28be7e088d10b4720cce8ef3a
Patriciah Asaji <triciah12@gmail.com>
2025-05-21 13:51:36 +0300

    commit exercise5.py

---
+++ Test.py
@@ -0,0 +1,8 @@
+class Animal:
+    def __init__(self, name, family, legs):
+        self.name = name
+        self.family = family
+        self.legs = legs
+        self.full_name = f'{name} {family}'
+        class cat(Animal)
+        
---
+++ wk1/Exercise5.py
@@ -0,0 +1,6 @@
+name="patriciah"
+age=30
+
+shoe_size=40
+info=f"my name is {name}.{age}.{shoe_size}"
+print(info)
