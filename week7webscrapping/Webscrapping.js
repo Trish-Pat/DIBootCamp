74e91e6b52d7f217363a41aea075cad21e759984
Patriciah <triciah12@gmail.com>
2025-07-13 15:06:41 +0300

    Initial commit

---
+++ week7webscrapping/Webscrapping.js
@@ -0,0 +1,12 @@
+let myString = "Hello, JavaScript!"; // String
+let myNumber = 42;                  // Number
+let myBoolean = true;               // Boolean
+let myUndefined;                    // Undefined
+let myNull = null;                  // Null
+
+console.log(myString, "Hello, Javascript",);
+console.log(myNumber, 42);
+console.log(myBoolean, true);
+console.log(myUndefined, undefined);
+console.log(myNull, null);
+
